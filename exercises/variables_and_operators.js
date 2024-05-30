75decce9ffbdfd56c1e254899777a9786c29e270
Marani-Ignacio <ignacio.marani.71@gmail.com>
2024-05-30 19:26:01 -0300

    Realice la lista de ejercicios de javascript

---
+++ exercises/variables_and_operators.js
@@ -0,0 +1,17 @@
+//1-a)
+var primerNumero = 15;
+var segundoNumero = 20;
+suma = primerNumero + segundoNumero;
+console.log(suma);
+
+//1-b)
+var primerPalabra = "Montaña";
+var segundaPalabra = "rusa";
+palabraCompuesta = primerPalabra + segundaPalabra;
+console.log(palabraCompuesta);
+
+//1-c)
+var primerPalabra = "Ignacio";
+var segundaPalabra = "Marani";
+var palabraCompuesta = primerPalabra.length + segundaPalabra.length;
+console.log(palabraCompuesta);
